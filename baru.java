2becfb44ca67d5bc4ec6585bdcf28f03ae74d05c
alfinaufal12 <alfinaufal.fal@gmail.com>
2020-01-15 16:16:38 +0700

    tambahan

---
+++ baru.java
@@ -0,0 +1,15 @@
+public class baru {
+    
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[]args){
+        String nama = "alfi naufal";
+        String alamat = "cibubur";
+        String kelas = "3AEU2";
+        int umur = 18;
+        
+    System.out.println("Nama :" + nama + "\nAlamat :" + alamat + "\nKelas :" + kelas + "\nUmur :" + umur);
+}
+}
